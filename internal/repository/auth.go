b913866ffbc2bf51828d3253cbc8dd305c7561af
zhdanila <daniazhurbaaa@gmail.com>
2025-04-12 21:38:56 +0300

    Move token methods to auth interfaces

---
+++ internal/repository/auth.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"finly-backend/internal/domain"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+const UsersTable = "users"
+
+type AuthRepository struct {
+	postgres *sqlx.DB
+	redis    *redis.Client
+}
+
+func NewAuthRepository(postgres *sqlx.DB, redis *redis.Client) *AuthRepository {
+	return &AuthRepository{
+		postgres: postgres,
+		redis:    redis,
+	}
+}
+
+func (a *AuthRepository) Register(ctx context.Context, email, passwordHash, firstName, lastName string) (string, error) {
+	query := fmt.Sprintf("INSERT INTO %s (email, password_hash, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING id", UsersTable)
+
+	var userID string
+	err := a.postgres.QueryRowContext(ctx, query, email, passwordHash, firstName, lastName).Scan(&userID)
+	if err != nil {
+		return "", err
+	}
+
+	return userID, nil
+}
+
+func (a *AuthRepository) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	query := fmt.Sprintf("SELECT password_hash FROM %s WHERE email = $1", UsersTable)
+
+	var user domain.User
+	if err := a.postgres.GetContext(ctx, &user, query, email); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (a *AuthRepository) GetUserByID(ctx context.Context, id string) (*domain.User, error) {
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id = $1", UsersTable)
+
+	var user domain.User
+	if err := a.postgres.GetContext(ctx, &user, query, id); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (a *AuthRepository) AddTokenToBlacklist(ctx context.Context, token string, ttlSeconds float64) error {
+	return a.redis.Set(ctx, token, "blacklisted", time.Duration(ttlSeconds)*time.Second).Err()
+}
+
+func (a *AuthRepository) IsTokenBlacklisted(ctx context.Context, token string) (bool, error) {
+	val, err := a.redis.Get(ctx, token).Result()
+	if errors.Is(err, redis.Nil) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return val == "blacklisted", nil
+}
+
+func (a *AuthRepository) RemoveToken(ctx context.Context, token string) error {
+	return a.redis.Del(ctx, token).Err()
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,21 +3,17 @@ package repository
 import (
 	"context"
 	"finly-backend/internal/domain"
-	"finly-backend/internal/repository/postgres"
-	redis2 "finly-backend/internal/repository/redis"
 	"github.com/jmoiron/sqlx"
 	"github.com/redis/go-redis/v9"
 )
 
 type Repository struct {
 	Auth
-	Token
 }
 
-func NewRepository(db *sqlx.DB, redis *redis.Client) *Repository {
+func NewRepository(postgres *sqlx.DB, redis *redis.Client) *Repository {
 	return &Repository{
-		Auth:  postgres.NewAuthRepository(db),
-		Token: redis2.NewTokenRepository(redis),
+		Auth: NewAuthRepository(postgres, redis),
 	}
 }
 
@@ -25,9 +21,7 @@ type Auth interface {
 	Register(ctx context.Context, email, passwordHash, firstName, lastName string) (string, error)
 	GetUserByEmail(ctx context.Context, email string) (*domain.User, error)
 	GetUserByID(ctx context.Context, id string) (*domain.User, error)
-}
 
-type Token interface {
 	AddTokenToBlacklist(ctx context.Context, token string, ttlSeconds float64) error
 	IsTokenBlacklisted(ctx context.Context, token string) (bool, error)
 	RemoveToken(ctx context.Context, token string) error
