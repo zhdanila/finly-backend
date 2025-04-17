22924bccb542aa636b2856c74456cc2484c09eb7
zhdanila <daniazhurbaaa@gmail.com>
2025-04-17 18:56:29 +0300

    Add health endpoint

--- internal/transport/http/router/router.go
+++ internal/transport/http/router/router.go
@@ -4,6 +4,8 @@ import (
 	"finly-backend/internal/service"
 	"finly-backend/internal/transport/http/handler"
 	"finly-backend/pkg/server"
+	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func RegisterRoutes(server *server.Server, services *service.Service) {
@@ -12,4 +14,8 @@ func RegisterRoutes(server *server.Server, services *service.Service) {
 	handler.NewCategory(services).Register(server)
 	handler.NewBudget(services).Register(server)
 	handler.NewTransaction(services).Register(server)
+
+	server.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
 }
