e7c3ab5d333e5055d0d6df95098a8a3dfc636c36
zhdanila <daniazhurbaaa@gmail.com>
2025-04-17 18:50:39 +0300

    Remove printing variables

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,31 +2,9 @@ package main
 
 import (
 	"finly-backend/internal/app/bootstrap"
-	"fmt"
-	"os"
 )
 
 func main() {
-	httpPort := os.Getenv("HTTP_PORT")
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dbSSLMode := os.Getenv("DB_SSLMODE")
-	redisHost := os.Getenv("REDIS_HOST")
-	redisPort := os.Getenv("REDIS_PORT")
-
-	fmt.Println("Starting Finly Backend...")
-	fmt.Printf("HTTP Port: %s\n", httpPort)
-	fmt.Printf("Database Username: %s\n", dbUsername)
-	fmt.Println("Database Password:]", dbPassword)
-	fmt.Printf("Database Host: %s\n", dbHost)
-	fmt.Printf("Database Port: %s\n", dbPort)
-	fmt.Printf("Database Name: %s\n", dbName)
-	fmt.Printf("Database SSL Mode: %s\n", dbSSLMode)
-	fmt.Printf("Redis Host: %s\n", redisHost)
-	fmt.Printf("Redis Port: %s\n", redisPort)
 	// Initialize the website application
 	bootstrap.Website()
 }
